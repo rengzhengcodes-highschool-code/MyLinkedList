21c68f9a692d5d98f9623d02cf70f227dcb0db18
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-06 13:13:57 -0500

    Added size() function.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,7 +5,11 @@ public class MyLinkedList{
 		/*create a constructor*/
 		size = 0;
 	}
-	public int size();
+
+	public int size() {
+		return size;
+	}
+	
 	public boolean add(String value);
 	public boolean add(int index, String value);
 	public String get(int index);
