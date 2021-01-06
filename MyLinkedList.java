fef54563947469844ab588243e1130d885e56b1e
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-06 13:19:31 -0500

    Added add functionality.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -9,8 +9,23 @@ public class MyLinkedList{
 	public int size() {
 		return size;
 	}
-	
-	public boolean add(String value);
+
+	public boolean add(String value) {
+		try {
+			Node insert = new Node(value);
+
+			if (size == 0) {
+				start = end = insert; //all point towards same node instance as head + tail.
+			} else {
+				end.nextSet(insert);
+			}
+
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 	public boolean add(int index, String value);
 	public String get(int index);
 	public String set(int index, String value);
