c87faa8da223d2945bcf84b06c4eccae4c35987c
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-11 09:36:59 -0500

    Added a static and random barrage test.

--- MyLinkedListTester.java
+++ MyLinkedListTester.java
@@ -10,9 +10,25 @@ public class MyLinkedListTester extends TesterMethods {
 	public static boolean sizeTester(int tests) {
 		tester("size");
 		boolean fail = false;
-
+		//tests all size numbers from 0 to tests - 1
 		for (int test = 0; test < tests; test++) {
-			int sizeExpected = TesterMethods.randInt(100);
+			int sizeExpected = test;
+			MyLinkedList analyte = new MyLinkedList();
+
+			for (int amount = 0; amount < sizeExpected; amount++) {
+				analyte.add(Integer.toString(amount));
+			}
+
+			if (sizeExpected == analyte.size()) {
+				//TesterMethods.passMessage(test);
+			} else {
+				TesterMethods.errorMessage(test, sizeExpected, analyte.size());
+				fail = true;
+			}
+		}
+		//tests random sizes test times up to test size
+		for (int test = 0; test < tests; test++) {
+			int sizeExpected = TesterMethods.randInt(tests);
 			MyLinkedList analyte = new MyLinkedList();
 
 			for (int amount = 0; amount < sizeExpected; amount++) {
